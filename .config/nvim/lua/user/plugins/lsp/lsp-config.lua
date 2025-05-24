77bcefadfbfd5180c5e2ea38324b4292552f5be4
Valentin Berlin <valentin.berlin@qonto.com>
2025-05-24 17:18:13 +0200

    update mason and lsp-config setup

--- .config/nvim/lua/user/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/user/plugins/lsp/lsp-config.lua
@@ -6,10 +6,6 @@ return {
     { "antosha417/nvim-lsp-file-operations", config = true },
   },
   config = function()
-    local lspconfig = require("lspconfig")
-    local mason_lspconfig = require("mason-lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
     local keymap = vim.keymap.set
 
     vim.api.nvim_create_autocmd("LspAttach", {
@@ -28,8 +24,6 @@ return {
       end,
     })
 
-    local capabilities = cmp_nvim_lsp.default_capabilities()
-
     -- Change the Diagnostic symbols in the sign column (gutter)
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 
@@ -37,71 +31,5 @@ return {
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
-
-    mason_lspconfig.setup_handlers({
-      -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
-      -- custom configs for language servers
-      ["stylelint_lsp"] = function()
-        lspconfig["stylelint_lsp"].setup({
-          capabilities = capabilities,
-          filetypes = {
-            "css",
-            "sass",
-            "scss",
-            "less",
-          },
-        })
-      end,
-      ["emmet_ls"] = function()
-        lspconfig["emmet_ls"].setup({
-          capabilities = capabilities,
-          filetypes = {
-            "html",
-            "typescriptreact",
-            "javascriptreact",
-            "css",
-            "sass",
-            "scss",
-            "less",
-            "svelte",
-          },
-        })
-      end,
-      ["lua_ls"] = function()
-        lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { "vim" },
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-            },
-          },
-        })
-      end,
-      ["ember"] = function()
-        lspconfig["ember"].setup({
-          capabilities = capabilities,
-          settings = {
-            ember = {
-              templateLinting = true,
-              ignoreFilesOnServerStart = {
-                "node_modules/**",
-                "tmp/**",
-              },
-            },
-          },
-        })
-      end,
-    })
   end,
 }
